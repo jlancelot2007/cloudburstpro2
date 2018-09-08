9bb437c0ec79cde3e358588efffeb9ff3e0f86f7
Andrew Plater <aplater@myfizz.biz>
2018-09-08 12:25:26 +1000

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import pic01 from '../images/pic01.jpg'
+import pic02 from '../images/pic02.jpg'
+import pic03 from '../images/pic03.jpg'
+
+class Main extends React.Component {
+  render() {
+
+    let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
+
+    return (
+      <div id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
+
+        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Intro</h2>
+          <span className="image main"><img src={pic01} alt="" /></span>
+          <p>Amazon Web Services are a great way to build your business in a secure, low cost, low maintenance environment that is highly available and highly scalable. With so many services to pick from, it can seem overwhelming to try and get started. What you need is a partner who can blend things together in a seamless way. By the way, check out my <a href="#work">awesome work</a>.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+          {close}
+        </article>
+
+        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Work</h2>
+          <span className="image main"><img src={pic02} alt="" /></span>
+          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
+          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+          {close}
+        </article>
+
+        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">About</h2>
+          <span className="image main"><img src={pic03} alt="" /></span>
+          <p>My name is Andrew Plater. I have been working with AWS and related services such as Slack for about three years. I hold an AWS Solutions Architect Associate certification and love to build serverless solutions to business problems.</p>
+          {close}
+        </article>
+
+        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Contact</h2>
+          <form method="post" action="#">
+            <div className="field half first">
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className="field half">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" id="email" />
+            </div>
+            <div className="field">
+              <label htmlFor="message">Message</label>
+              <textarea name="message" id="message" rows="4"></textarea>
+            </div>
+            <ul className="actions">
+              <li><input type="submit" value="Send Message" className="special" /></li>
+              <li><input type="reset" value="Reset" /></li>
+            </ul>
+          </form>
+          <ul className="icons">
+            <li><a href="https://twitter.com/jlancelot2007" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+            <li><a href="https://www.facebook.com/andrew.plater.9" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+            <li><a href="https://www.linkedin.com/in/andrew-plater-56122790/" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
+            <li><a href="https://github.com/jlancelot2007" className="icon fa-github"><span className="label">GitHub</span></a></li>
+          </ul>
+          {close}
+        </article>
+
+      </div>
+    )
+  }
+}
+
+Main.propTypes = {
+  route: PropTypes.object,
+  article: PropTypes.string,
+  articleTimeout: PropTypes.bool,
+  onCloseArticle: PropTypes.func,
+  timeout: PropTypes.bool
+}
+
+export default Main
