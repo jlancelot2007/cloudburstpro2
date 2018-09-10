778b2799ed42593a45c6c0582391bd9a6cf861a3
Andrew Plater <aplater@myfizz.biz>
2018-09-10 17:23:52 +1000

    contact form

--- src/components/Main.js
+++ src/components/Main.js
@@ -50,7 +50,7 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form name="contact" method="post"  data-netlify="true" data-netlify-honeypot="botfield">>
+          <form name="contact" method="post"  data-netlify="true" data-netlify-honeypot="botfield">
             <input type="hidden" name="bot-field" />
             <div className="field half first">
               <label htmlFor="name">Name</label>
