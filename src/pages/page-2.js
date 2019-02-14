920de2b37eedd88514620abc1d68d5ca6a121bda
Andrew Plater <aplater@myfizz.biz>
2019-02-14 13:20:54 +1100

    Tested with Stripe

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,8 +5,11 @@ import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Congratulations</h1>
+    <p>Your purchase from Cloudburstpro has been successful!</p>
+    <p>Check your e-mail address for the receipt that has been mailed to you</p>
+    <p>If a receipt has not been received please contact us by e-mail to request a duplicate.</p>
+    <p>Once again thank you for your business - it is a pleasure to serve you.</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
